ab8ad4cd51b33dc601d60dbdfe4934f66cebfe96
Tobi2933 <oriyomidavid942@gmail.com>
2023-06-16 18:34:26 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * Rerurn: Always 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,6 +13,7 @@ int main(void)
 
 	for (p = 0; p <= 98; p++)
 	{
+
 	for (q = p + 1; p <= 99; q++)
 	{
 	putchar((p / 10) + '0');
