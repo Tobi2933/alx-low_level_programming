3a9ea66cb843f348fd0f6b3f4638bbf7c37da4bc
Tobi2933 <oriyomidavid942@gmail.com>
2023-06-20 05:01:23 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,12 @@
 int _putchar(char c);
 
-void print_alphsbet(void);
+void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
 int _islower(int c);
 
-int _isaplha(int c);
+int _isalpha(int c);
 
 int print_sign(int n);
 
