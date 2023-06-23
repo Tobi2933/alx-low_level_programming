ecac6f6f513802dc2d28d3a369e8d7d48ae347a5
Tobi2933 <oriyomidavid942@gmail.com>
2023-06-22 19:59:14 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Test function for positive or negative
+ * main - Tests function that prints if interger is positive or negative
  * Return: 0
  */
 int main(void)
