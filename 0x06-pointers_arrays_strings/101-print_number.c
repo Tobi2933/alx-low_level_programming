5b5d3f4437bf74fe75d39573eb78b2fa16894096
Tobi2933 <oriyomidavid942@gmail.com>
2023-06-29 14:20:02 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
+
 /**
+
  * print_number - print numbers chars
+
  * @n: integer params
+
  * Return: 0
+
  */
+
 
 void print_number(int n)
 {
+
 	unsigned int n1;
 
 	n1 = n;
