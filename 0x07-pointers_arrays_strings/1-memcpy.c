ce672f3d094c8a777dfb56ce9f0460e92ce333f2
Tobi2933 <oriyomidavid942@gmail.com>
2023-07-03 11:23:58 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _memcpy - 
- * @dest: 
- * @src: 
- * *@n: 
+ * _memcpy - Entry point 
+ * @dest: input
+ * @src: input
+ * *@n: input
  *
- * Return: 
+ * Return: Always 0 (success)
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
