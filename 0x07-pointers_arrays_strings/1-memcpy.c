56e0a8e7e29fe039e6b2fd9fe5ad17cbfff44f70
Tobi2933 <oriyomidavid942@gmail.com>
2023-07-03 12:04:05 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,19 @@
 #include "main.h"
+
 /**
- * _memcpy - Entry point 
+ * _memcpy - Entry point
  * @dest: input
  * @src: input
- * *@n: input
- *
- * Return: Always 0 (success)
+ * @n: input
+ * Return: Always 0 (Success)
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int r = 0;
-	int i = n;
+	unsigned int i;
 
-	for (; r < i; r++)
-	{
-		dest[r] = src[r];
-		n--;
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
 	return (dest);
 }
